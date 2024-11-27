694538a040d2abbc3403b7e3bb98aa2152bd8de5
Hiago Zavarize <hiagozavarizex2@gmail.com>
2024-11-27 20:22:03 -0300

    feat: adding images and configuring app_images class

---
+++ lib/utils/app_images.dart
@@ -0,0 +1,6 @@
+abstract class AppImages {
+  static const String trucoIntro = "lib/assets/intro.png";
+  static const String botaoTruco = "lib/assets/botão de truco.png";
+  static const String cardsNewGame = "lib/assets/cards-new-game.png";
+  static const String cardsContinue = "lib/assets/cards-continue-game.png";
+}
