a46050d90d0352831eab409aa0c3f67289428b08
Hiago Zavarize <hiagozavarizex2@gmail.com>
2025-01-21 14:30:01 -0300

    feat: configuring main file and adding routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:provider/provider.dart';
+import 'package:truco/screens/home.dart';
+import 'package:truco/screens/personalization.dart';
+import 'package:truco/controllers/score_colors_state_maneger.dart';
+import 'package:truco/screens/settings.dart';
+import 'package:truco/utils/utils.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'intro.dart';
 
 void main() {
-  runApp(const MainApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
+  runApp(ChangeNotifierProvider(
+      create: (context) => ScoreColorsStateManeger(), child: const MainApp()));
 }
 
 class MainApp extends StatelessWidget {
@@ -9,12 +21,31 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
-    );
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        navigatorKey: navigatorKey,
+        initialRoute: TrucoIntro.screenName,
+        routes: {
+          TrucoIntro.screenName: (_) => const TrucoIntro(),
+          HomeScreen.screenName: (_) => HomeScreen(),
+          Settings.screenName: (_) => Settings(),
+          Personalization.screenName: (_) => Personalization(),
+        },
+        theme: ThemeData(
+          textTheme: GoogleFonts.poppinsTextTheme(),
+          scaffoldBackgroundColor: AppColors.backgroundColor,
+          primaryColor: Colors.white,
+          useMaterial3: false,
+          appBarTheme: AppBarTheme(
+            backgroundColor: AppColors.appBarColor,
+            elevation: .0,
+            foregroundColor: Colors.black,
+            titleTextStyle: TextStyle(
+                fontWeight: FontWeight.bold, color: Colors.white, fontSize: 24),
+            iconTheme: IconThemeData(
+              size: 34,
+            ),
+          ),
+        ));
   }
 }
